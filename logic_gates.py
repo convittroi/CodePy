e5f91ddb13398f1f26619ab01afe57385912ea4e
convittroi <chuongsuper000@gmail.com>
2022-05-10 11:34:24 +0700

    add hypercube

---
+++ logic_gates.py
@@ -0,0 +1,47 @@
+def AND(a,b):
+    if a==1 and b==1:
+        return 1
+    else:
+        return 0
+
+def NAND (a, b): 
+    if a == 1 and b == 1: 
+        return 0
+    else: 
+        return 1
+
+def OR(a, b): 
+    if a == 1: 
+        return 1
+    elif b == 1: 
+        return 1
+    else: 
+        return 0
+
+def XOR (a, b): 
+    if a != b: 
+        return 1
+    else: 
+        return 0
+
+def NOT(a): 
+    if(a == 0): 
+        return 1
+    elif(a == 1): 
+        return 0
+
+def NOR(a, b): 
+    if(a == 0) and (b == 0): 
+        return 1
+    elif(a == 0) and (b == 1): 
+        return 0
+    elif(a == 1) and (b == 0): 
+        return 0
+    elif(a == 1) and (b == 1): 
+        return 0
+
+def XNOR(a,b): 
+    if(a == b): 
+        return 1
+    else: 
+        return 0
--- pygame_tutorial.py
+++ pygame_tutorial.py
@@ -4,25 +4,40 @@ win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Test Pygame")
 x=50
 y=50
-rad =5
-vel= 5
+rad =20
+vel= 10
 run=True
+isjump = False
+jumpcount=10
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(60)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
     pygame.draw.circle(win,(255,0,0),(x,y),rad)
     pygame.display.update()
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x>25:
         x-=vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x<475:
         x +=vel
-    if keys[pygame.K_DOWN]:
-        y += vel
-    if keys[pygame.K_UP]:
-        y -= vel
+    if not(isjump):
+        if keys[pygame.K_DOWN] and y<475:
+            y += vel
+        if keys[pygame.K_UP] and y>25:
+            y -= vel
+        if keys[pygame.K_SPACE]:
+            isjump =True
+    else:
+        if jumpcount >=-10 :
+            neg =1
+            if jumpcount<0:
+                neg=-1
+            y -= (jumpcount)**2 *0.5 *neg
+            jumpcount-=1
+        else:
+            isjump=False
+            jumpcount=10
     win.fill((0,0,0))
 pygame.quit()
 
